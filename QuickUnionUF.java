e225f582be2073f0e479cbc7abe40c58cdc45671
wadood786 <71840338+wadood786@users.noreply.github.com>
2020-11-29 14:04:04 +0500

    Update QuickUnionUF.java

--- QuickUnionUF.java
+++ QuickUnionUF.java
@@ -2,14 +2,7 @@ public class QuickUnionUF {
     private int[] parent;  // parent[i] = parent of i
     private int count;     // number of components
 
-    /**
-     * Initializes an empty union-find data structure with
-     * {@code n} elements {@code 0} through {@code n-1}. 
-     * Initially, each elements is in its own set.
-     *
-     * @param  n the number of elements
-     * @throws IllegalArgumentException if {@code n < 0}
-     */
+    
     public QuickUnionUF(int n) {
         parent = new int[n];
         count = n;
@@ -18,22 +11,11 @@ public class QuickUnionUF {
         }
     }
 
-    /**
-     * Returns the number of sets.
-     *
-     * @return the number of sets (between {@code 1} and {@code n})
-     */
+    
     public int count() {
         return count;
     }
-  
-    /**
-     * Returns the canonical element of the set containing element {@code p}.
-     *
-     * @param  p an element
-     * @return the canonical element of the set containing {@code p}
-     * @throws IllegalArgumentException unless {@code 0 <= p < n}
-     */
+ 
     public int find(int p) {
         validate(p);
         while (p != parent[p])
@@ -49,31 +31,13 @@ public class QuickUnionUF {
         }
     }
 
-    /**
-     * Returns true if the two elements are in the same set.
-     * 
-     * @param  p one element
-     * @param  q the other element
-     * @return {@code true} if {@code p} and {@code q} are in the same set;
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
-     * @deprecated Replace with two calls to {@link #find(int)}.
-     */
+    
     @Deprecated
     public boolean connected(int p, int q) {
         return find(p) == find(q);
     }
 
-    /**
-     * Merges the set containing element {@code p} with the 
-     * the set containing element {@code q}.
-     *
-     * @param  p one element
-     * @param  q the other element
-     * @throws IllegalArgumentException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
-     */
+ 
     public void union(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
@@ -82,15 +46,7 @@ public class QuickUnionUF {
         count--;
     }
 
-    /**
-     * Reads an integer {@code n} and a sequence of pairs of integers
-     * (between {@code 0} and {@code n-1}) from standard input, where each integer
-     * in the pair represents some element;
-     * if the elements are in different sets, merge the two sets
-     * and print the pair to standard output.
-     * 
-     * @param args the command-line arguments
-     */
+   
     public static void main(String[] args) {
         int n = StdIn.readInt();
         QuickUnionUF uf = new QuickUnionUF(n);
